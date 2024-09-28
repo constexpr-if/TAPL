9d8621625628c61a7728db7db9f93adf1b3753e8
constexpr-if <constexpr12@gmail.com>
2024-09-28 23:19:57 +0900

    Use 24.05 instead of unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Template Flake for OCaml Projects";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs }:
